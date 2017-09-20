622b9f6cb9d977aa17847c30592b6a9b7b4653d4
Pulppy <thocon7278@gmail.com>
2017-09-20 15:03:21 +0700

    Commit Change

--- aura/CallScriptNAOptComp/CallScriptNAOptCompHelper.js
+++
@@ -1,7 +0,0 @@
-({
-    navigateToTaskObject: function(component, recordId){
-        var navEvt= $A.get("e.force:navigateToSObject");
-        navEvt.setParams({"recordId": recordId});
-        navEvt.fire();
-   	}
-})
